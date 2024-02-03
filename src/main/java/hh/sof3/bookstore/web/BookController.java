78455a15ac4d585db20524ce0cebbe7a7ed82e99
Markus Väänänen <markus.vaananen@gmail.com>
2024-02-03 19:32:08 +0200

    first commit

---
+++ src/main/java/hh/sof3/bookstore/web/BookController.java
@@ -0,0 +1,18 @@
+package hh.sof3.bookstore.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.ModelAttribute;
+
+@Controller
+public class BookController {
+    
+    @RequestMapping(value="/index", method=RequestMethod.GET)
+    public String showIndex() {
+        return "index";
+    }
+
+
+}
